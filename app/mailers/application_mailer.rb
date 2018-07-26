ee2a90d1632c37d5752120e9ade3b6b224ed95c6
vincent535 <taiuka3@icloud.com>
2018-07-26 20:54:58 +0900

    mail sending in development implementation final

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: "ukaukauka726@gmail.com"
   layout 'mailer'
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,16 +60,16 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port:3000 }
-
+  config.action_mailer.default_options = { from: 'ukaukauka726@gmail.com' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
    enable_starttls_auto: true,
-   address: 'smtp.gmail.com',
+   address: "smtp.gmail.com",
    port: 587,
-   dmain: 'example.com',
-   user_name: 'ukaukauka726@gmail.com',
-   password: 'moyahshiramen-3',
-   authentication: 'plain'
+   dmain: 'http://localhost:3000/',
+   user_name: "ukaukauka726@gmail.com",
+   password: "gqfbjeymsfegfobm",
+   authentication: 'login'
    }
 end
