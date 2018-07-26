bf25d95f513099ae5d156555833a9e5afe99332a
vincent535 <taiuka3@icloud.com>
2018-07-26 18:56:33 +0900

    mail sending implementation again

--- config/routes.rb
+++ config/routes.rb
@@ -31,7 +31,7 @@ end
   resources :users, :only => [:show]
   #resources :users, :only => [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  if Rails.env.development?
-    mount LetterOpenerWeb::Engine, at: "/letter_opener"
-  end
+  #if Rails.env.development?
+    #mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  #end
 end
